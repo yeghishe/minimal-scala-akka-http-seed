d0b96b24c9a5c2c367550631c1e060abb67f875c
yeghishe <ypiruzyan@gmail.com>
2015-09-02 18:23:53 -0700

    Small change

--- src/main/scala/io/github/yeghishe/Main.scala
+++ src/main/scala/io/github/yeghishe/Main.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 
 object Main extends App with Config with StatusService {
-  override protected implicit def system: ActorSystem = ActorSystem()
+  override protected implicit val system: ActorSystem = ActorSystem()
   override protected implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   Http().bindAndHandle(routes, httpInterface, httpPort)
